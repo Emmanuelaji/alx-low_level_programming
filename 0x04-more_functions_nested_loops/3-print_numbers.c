b19c82072e4e786923309b4bdac7f2139998991d
EMMANUEL AJI AGBO <31956253+Emmanuelaji@users.noreply.github.com>
2022-09-15 18:54:39 +0000

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,11 @@
  */
 void print_numbers(void)
 {
-	int i;
-	for (i = 48; i < 58; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+		int i;
+
+		for (i = 48; i < 58; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
 }
