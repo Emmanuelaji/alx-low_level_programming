dd8424bd327989d8fa308f74d4d65f50e9713f64
emmanuelaji <emmanuelaji32@gmail.com>
2022-09-19 16:47:06 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a  string to stdout
+ * @str: string to print
+ * Return: void
+  */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i += 1;
+	}
+	_putchar('\n');
+}
