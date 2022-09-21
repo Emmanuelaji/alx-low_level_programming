37db0bdca517256b5d42dd9c89288301d8cf72af
EMMANUEL AJI AGBO <31956253+Emmanuelaji@users.noreply.github.com>
2022-09-21 18:18:49 -0400

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,9 +15,12 @@ char *cap_string(char *s)
 		{
 			if (i == 0)
 				*(s + i) -= 'a' - 'A';
-			else																	{																		for (j = 0; j <= 12; j++)
+			else
+			{
+				for (j = 0; j <= 12; j++)
 				{
-					if (a[j] == *(s + i - 1))																																	*(s + i) -= 'a' - 'A';
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
 				}
 			}
 		}
