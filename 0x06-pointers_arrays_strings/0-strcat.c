186e08c81aa55f50d2babc2ed6e9ad7da73b4deb
emmanuelaji <emmanuelaji32@gmail.com>
2022-09-21 14:19:39 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
  * _strcat - appends src to the dest string
  * @dest: string to append by src
  * @src: string to append to dest
- * return: address of dest
+ * Return: address of dest
  */
 char *_strcat(char *dest, char *src)
 {
