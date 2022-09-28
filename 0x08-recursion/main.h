d44fcb0bf74664b3edf90989a804b3eb441be253
emmanuelaji <emmanuelaji32@gmail.com>
2022-09-28 17:14:29 -0400

    put recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+void _puts_recursion(char *s);
+void _puts_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _is_prime_number(int n);
+int _putchar(char c);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
