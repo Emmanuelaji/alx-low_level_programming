e60bc6f712e202ff1a6c8b8aa382bf07a4aafc9b
EMMANUEL AJI AGBO <31956253+Emmanuelaji@users.noreply.github.com>
2022-10-13 21:21:03 -0400

    main file

--- 0x10-variadic_functions/0-main.c
+++ 0x10-variadic_functions/0-main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
@@ -7,11 +6,6 @@
  */
 int main(void)
 {
-	int sum;
-	
-	sum = sum_them_all(2, 98, 1024);
-	printf("%d\n", sum);
-	sum = sum_them_all(4, 98, 1024, 402, -1024);
-	printf("%d\n", sum);
+	print_numbers(", ", 4, 0, 98, -1024, 402);
 	return (0);
 }
