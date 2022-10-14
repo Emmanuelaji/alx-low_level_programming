1e0bbccd4ab5677432b97d701097be0729c64f06
EMMANUEL AJI AGBO <31956253+Emmanuelaji@users.noreply.github.com>
2022-10-13 21:04:24 -0400

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - testing fuction for our program
+ * @ac: arguments passed count
+ * @av: array of arguments passed
+ * Return: 0 success
+ */
+int main(int ac, char *av[])
+{
+	int (*math)(int, int);
+
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!(av[2][0] == '+' || av[2][0] == '-' || av[2][0] == '*'
+				|| av[2][0] == '/' || av[2][0] == '%') || strlen(av[2]) > 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((av[2][0] == '/' || av[2][0] == '%') && atoi(av[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	math = get_op_func(av[2]);
+	printf("%d\n", math(atoi(av[1]), atoi(av[3])));
+	return (0);
+}
